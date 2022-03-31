78dbcfb43b3cdbfc742b731ba80b7237a5b112ef
José Arnaldo <josearnaldo.net@gmail.com>
2022-03-30 22:44:07 -0300

    Acessibilidade Web

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,16 @@
-console.log("javascript carregado")
+$(".AltoContraste").click(function() {
+    $("html, body").toggleClass("contraste");
+    console.log("Ativa o contraste");
+});
+
+$(".FonteMaior").click(function() {
+    // $("html, body").toggleClass("contraste");
+    // $('.changeMe').css("font-size", $(this).val() + "px");
+    $("html, body").css("font-size", "50px");
+    console.log("Fonte Maior");
+});
+
+$(".FonteMenor").click(function() {
+    $("html, body").css("font-size", "25px");
+    console.log("Fonte Menor");
+});
