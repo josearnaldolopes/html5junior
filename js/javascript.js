83a789c3445353448c1aefe26dcdec6cd3857f81
José Arnaldo <josearnaldo.net@gmail.com>
2022-03-30 22:51:17 -0300

    Revert "Acessibilidade Web"
    This reverts commit 78dbcfb43b3cdbfc742b731ba80b7237a5b112ef.

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,1 @@
-$(".AltoContraste").click(function() {
-    $("html, body").toggleClass("contraste");
-    console.log("Ativa o contraste");
-});
-
-$(".FonteMaior").click(function() {
-    // $("html, body").toggleClass("contraste");
-    // $('.changeMe').css("font-size", $(this).val() + "px");
-    $("html, body").css("font-size", "50px");
-    console.log("Fonte Maior");
-});
-
-$(".FonteMenor").click(function() {
-    $("html, body").css("font-size", "25px");
-    console.log("Fonte Menor");
-});
+console.log("javascript carregado")
